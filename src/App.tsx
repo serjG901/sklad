0acfae479b97c2a5ddbd3a953858cd50df621f0f
serjG901 <70537823+serjG901@users.noreply.github.com>
2024-01-26 10:50:06 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -79,6 +79,44 @@ const handleOpenFile = async () => {
 
 }
 
+	async function verifyPermission(fileHandle, withWrite) {
+
+  const opts = {};
+
+  if (withWrite) {
+
+    opts.mode = "readwrite";
+
+  }
+
+
+
+  // Check if we already have permission, if so, return true.
+
+  if ((await fileHandle.queryPermission(opts)) === "granted") {
+
+    return true;
+
+  }
+
+
+
+  // Request permission to the file, if the user grants permission, return true.
+
+  if ((await fileHandle.requestPermission(opts)) === "granted") {
+
+    return true;
+
+  }
+
+
+
+  // The user did not grant permission, return false.
+
+  return false;
+
+	}
+
     return (
         <><button onClick={handleOpenFile}>open file</button> <br /><br />
             <div>Delete Item</div>
