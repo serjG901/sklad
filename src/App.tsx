00d8f8c31d2fbbca947adec2fc43a501237bfb61
serjG901 <70537823+serjG901@users.noreply.github.com>
2024-01-28 03:18:30 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
     };
     /* */
     return (
-        <>/*@ts-ignore*/
+        <>
             <div popover='auto' id='tmc-popover'>
                 <UpdateTmc
                     toUpdateTmc={toUpdateTmc}
